db70d6050ba518ac061d35e863ff0c88d903315b
unknown <bhatgare.mayank10@gmail.com>
2024-08-10 17:25:13 +0530

    stack updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         <div>
           <ul className="nav-links">
             <li><a href="#about">About</a></li>
-            <li><a href="#experience">Experience</a></li>
+            <li><a href="#experience">Stacks</a></li>
             <li><a href="#projects">Projects</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
